e1c1a3dbb2d722a055ff4455f98a395d31160959
Team8271 <31601375+Team8271@users.noreply.github.com>
2024-11-13 16:52:16 -0800

    TeleOp and Config rewrite
    Config/TeleOp rewritten w/ CheetoMan

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration.java
@@ -1,76 +1,81 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.hardware.Sensor;
-
-import com.qualcomm.hardware.motors.GoBILDA5202Series;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class Configuration {
     private LinearOpMode opMode;
 
-    public DcMotor FL, FR, BL, BR, Horz, Vert, In;
+    //Global Variables
+    public int vertMax = 1300, horzMax = 400;
+    public int intakeOnDistance = 200;
 
-    public Servo Flip, Red, Blue, Org;
+    public DcMotor fl, fr, bl, br, horizontalMotor, verticalMotor, intakeMotor;
 
-    public TouchSensor Max = null, Min = null;
+    public Servo flipServo, redServo, blueServo, boxServo;
 
+    public TouchSensor verticalLimiter, horizontalLimiter;
+
+    public IMU imu;
 
     public Configuration(LinearOpMode opMode){
         this.opMode=opMode;
     }
+
+
     public void init(){
         HardwareMap hwMap=opMode.hardwareMap;
 
-        FL = hwMap.get(DcMotor.class,"FL");
-        FL.setDirection(DcMotor.Direction.FORWARD);
-        FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fl = hwMap.get(DcMotor.class,"FL");
+        fl.setDirection(DcMotor.Direction.FORWARD);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        FR = hwMap.get(DcMotor.class,"FR");
-        FR.setDirection(DcMotorSimple.Direction.REVERSE);
-        FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr = hwMap.get(DcMotor.class,"FR");
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        BL = hwMap.get(DcMotor.class,"BL");
-        BL.setDirection(DcMotor.Direction.REVERSE);
-        BL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl = hwMap.get(DcMotor.class,"BL");
+        bl.setDirection(DcMotor.Direction.REVERSE);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        BR = hwMap.get(DcMotor.class,"BR");
-        BR.setDirection(DcMotor.Direction.FORWARD);
-        BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br = hwMap.get(DcMotor.class,"BR");
+        br.setDirection(DcMotor.Direction.FORWARD);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        Horz = hwMap.get(DcMotor.class,"Horz");
-        Horz.setDirection(DcMotor.Direction.FORWARD);
-        Horz.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        horizontalMotor = hwMap.get(DcMotor.class,"Horz");
+        horizontalMotor.setDirection(DcMotor.Direction.FORWARD);
+        horizontalMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        Vert = hwMap.get(DcMotor.class,"Vert");
-        Vert.setDirection(DcMotor.Direction.FORWARD);
-        Vert.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalMotor = hwMap.get(DcMotor.class,"Vert");
+        verticalMotor.setDirection(DcMotor.Direction.REVERSE);
+        verticalMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        In =hwMap.get(DcMotor.class, "In");
-        In.setDirection(DcMotorSimple.Direction.FORWARD);
-        In.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        intakeMotor = hwMap.get(DcMotor.class, "In");
+        intakeMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        verticalLimiter = hwMap.get(TouchSensor.class, "VertSens");
+        horizontalLimiter = hwMap.get(TouchSensor.class, "HorzSens");
 
+        flipServo = hwMap.get(Servo.class, "Flip");
+        redServo = hwMap.get(Servo.class, "Red");
+        blueServo = hwMap.get(Servo.class, "Blue");
+        boxServo = hwMap.get(Servo.class, "Box");
 
-        Max = hwMap.get(TouchSensor.class, "Max");
-
-        Min = hwMap.get(TouchSensor.class, "Min");
-
-
-
-        Flip = hwMap.get(Servo.class, "Flip");
-
-        Red = hwMap.get(Servo.class, "Red");
-
-        Blue = hwMap.get(Servo.class, "Blue");
-
-        Org = hwMap.get(Servo.class, "Org");
-
-
+        imu = hwMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot( //Mes swith me
+                        RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP
+                )
+        );
+        imu.initialize(parameters);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,74 +2,241 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name="Main Teleop")
 public class Teleop extends LinearOpMode {
     private Configuration robot;
+
+    //Main OpMode
     @Override
     public void runOpMode() {
-        robot=new Configuration(this);
+        robot = new Configuration(this);
         robot.init();
+
+        telemetry.addLine("Initialized");
         waitForStart();
+        telemetry.clearAll();
 
         while (opModeIsActive()){
-            //Main Drive Control
-            double axial = -gamepad1.left_stick_y;
-            double lateral = -gamepad1.right_stick_x;
-            double yaw = -gamepad1.left_stick_x;
+            //Driver 1
+            double axialControl = -gamepad1.left_stick_y;
+            double lateralControl = -gamepad1.right_stick_x;
+            double yawControl = -gamepad1.left_stick_x;
+            double mainThrottle = .2+(gamepad1.right_trigger*0.8);
+            boolean resetFCD = gamepad1.dpad_up;
 
-            double power = gamepad1.right_trigger;
+            //Driver 2
+            double horzControl = gamepad2.right_stick_x;
+            double vertControl = -gamepad2.left_stick_y;
+            boolean boxControl = gamepad2.left_trigger >.25;
+            boolean pincherControl = gamepad2.left_trigger>.25;
+            boolean reverseIntake = gamepad2.right_trigger>.25;
 
-            double leftFrontPower = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower = axial - lateral + yaw;
-            double rightBackPower = axial + lateral - yaw;
+            //Drivetrain
+            double axial = axialControl;
+            double lateral = lateralControl;
 
-            robot.FL.setPower(leftFrontPower * power);
-            robot.FR.setPower(rightFrontPower * power);
-            robot.BL.setPower(leftBackPower * power);
-            robot.BR.setPower(rightBackPower * power);
+            if (resetFCD){
+                robot.imu.resetYaw();
+            }
+
+            double gamepadRadians = Math.atan2(lateralControl, axialControl);
+            double gamepadHypot = Range.clip(Math.hypot(lateralControl, axialControl), 0, 1);
+            double robotRadians = -robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double targetRadians = gamepadRadians + robotRadians;
+            lateral = Math.sin(targetRadians)*gamepadHypot;
+            axial = Math.cos(targetRadians)*gamepadHypot;
+
+            double leftFrontPower = axialControl + lateralControl + yawControl;
+            double rightFrontPower = axialControl - lateralControl - yawControl;
+            double leftBackPower = axialControl - lateralControl + yawControl;
+            double rightBackPower = axialControl + lateralControl - yawControl;
+
+            robot.fl.setPower(leftFrontPower * mainThrottle);
+            robot.fr.setPower(rightFrontPower * mainThrottle);
+            robot.bl.setPower(leftBackPower * mainThrottle);
+            robot.br.setPower(rightBackPower * mainThrottle);
+
+            //Horizontal Slide
+            telemetry.addData("Horizontal Slide Pos",robot.horizontalMotor.getCurrentPosition());
+            telemetry.addData("Horizontal Slide Limiter",robot.horizontalLimiter.isPressed());
+
+            if(robot.horizontalLimiter.isPressed()) { // Slide bottomed out
+                if (horzControl<0) {
+                    horzControl = 0;
+                }
+                robot.horizontalMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                telemetry.addLine("Horizontal slide bottomed out!");
+            }
+            else if(robot.horizontalMotor.getCurrentPosition()>=robot.vertMax) { // Slide topped out
+                if(horzControl>0){
+                    horzControl = 0;
+                }
+            }
+
+
+            if(horzControl != 0){ //Moving
+                if(robot.horizontalMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
+                    robot.horizontalMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                }
+                robot.horizontalMotor.setPower(horzControl);
+                telemetry.addLine("Horizontal Slide moving..");
+            }
+            else { //stop and hold
+                if(robot.horizontalMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION){
+                    int targetPosToHold = robot.horizontalMotor.getCurrentPosition();
+                    if (targetPosToHold>robot.horzMax){
+                        targetPosToHold = 0;
+                    }
+                    robot.horizontalMotor.setTargetPosition(targetPosToHold);
+                    robot.horizontalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.horizontalMotor.setPower(0.5);
+                }
+                telemetry.addLine("Horizontal Slide holding..");
+            }
+
+            telemetry.addLine();
+
+            // Intake
+            if (robot.verticalMotor.getCurrentPosition()<=10 && //Retracted
+            robot.horizontalMotor.getCurrentPosition()<=10) {
+                robot.flipServo.setPosition(.99);
+                robot.intakeMotor.setPower(reverseIntake?-1:1);
+                telemetry.addLine("Intake retracted");
+            }
+            else if(robot.horizontalMotor.getCurrentPosition()>=robot.intakeOnDistance){ // Extended
+                robot.flipServo.setPosition(-.4);
+                robot.intakeMotor.setPower(reverseIntake?-1:1);
+                telemetry.addLine("Intake Extended");
+            }
+            else { //Gray zone
+                robot.flipServo.setPosition(.6);
+                robot.intakeMotor.setPower(reverseIntake?-1:.5);
+                telemetry.addLine("Intake waiting");
+            }
+            telemetry.addLine();
+
+            //Vertical Slide
+            telemetry.addData("Vertical Slide Position",robot.verticalMotor.getCurrentPosition());
+            telemetry.addData("Vertical Slide Limiter",robot.verticalLimiter.isPressed());
+
+            if(robot.verticalLimiter.isPressed()){ //slide bottomed out
+                if(vertControl<0){
+                    vertControl = 0;
+                }
+                robot.verticalMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                telemetry.addLine("Vertical slide bottomed out");
+            }
+            else if(robot.verticalMotor.getCurrentPosition()>=robot.vertMax){ // Slide topped out
+                if(vertControl>0){
+                    vertControl = 0;
+                }
+                telemetry.addLine("Vertical slide topped out");
+            }
+
+            if(vertControl != 0) { //Moving
+                if(robot.verticalMotor.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER){
+                    robot.verticalMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                }
+                robot.verticalMotor.setPower(vertControl);
+                telemetry.addLine("Vertical slide moving");
+            }
+            else { //Stop and hold
+                if(robot.verticalMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION){
+                    int targetPosToHold = robot.verticalMotor.getCurrentPosition();
+                    if(targetPosToHold>robot.vertMax){
+                        targetPosToHold = robot.vertMax;
+                    }
+                    robot.verticalMotor.setTargetPosition(targetPosToHold);
+                    robot.verticalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.verticalMotor.setPower(.5);
+                }
+                telemetry.addLine("Vertical slide holding...");
+            }
+
+            telemetry.addLine();
+
+            //Pincher
+            if(pincherControl){
+                //robot.Red.setPosition(0.5);
+                //robot.Blue.setPosition(0.5);
+                telemetry.addLine("Red and Blue closed");
+            }
+            else{
+                //robot.Red.setPosition(0.5);
+                //robot.Blue.setPosition(0.5);
+                telemetry.addLine("Red and Blue open");
+            }
+
+            telemetry.addLine();
+
+            //Box
+            if(boxControl && robot.verticalMotor.getCurrentPosition()>=100){
+                robot.boxServo.setPosition(-.99);
+                telemetry.addLine("Box flipped");
+            }
+            else{
+                robot.boxServo.setPosition(.6);
+                telemetry.addLine("Box retracted");
+            }
+
+            //Telemetry
+            telemetry.update();
+
+
+
+
+
+            /*
+            robot.fl.setPower(leftFrontPower * mainThrottle);
+            robot.fr.setPower(rightFrontPower * mainThrottle);
+            robot.bl.setPower(leftBackPower * mainThrottle);
+            robot.br.setPower(rightBackPower * mainThrottle);
 
             //Intake control
                 //A button runs motor in
                 if(gamepad2.a) {
-                    robot.In.setPower(0.5);
+                    robot.intakeMotor.setPower(0.5);
                 }
                 //Y runs motor out
                 if(gamepad2.y) {
-                    robot.In.setPower(-0.5);
+                    robot.intakeMotor.setPower(-0.5);
                 }
                 //x stops motor
                 if(gamepad2.x) {
-                   robot.In.setPower(0.0);
+                   robot.intakeMotor.setPower(0.0);
                }
 
             //Horizontal motor
                 //right joystick forward = forward
-                robot.Horz.setPower(gamepad2.right_stick_x);
+                robot.horizontalMotor.setPower(gamepad2.right_stick_x);
 
             //Vertical motor
                 //left joystick forward = up
 
 
-                if (!robot.Max.isPressed() && !robot.Min.isPressed())
+                if (!robot.verticalLimiter.isPressed() && !robot.horizontalLimiter.isPressed())
                 {
-                    robot.Vert.setPower(gamepad2.left_stick_y);
+                    robot.verticalMotor.setPower(gamepad2.left_stick_y);
                 }
                 else {
-                    if (robot.Max.isPressed()) {
+                    if (robot.verticalLimiter.isPressed()) {
                     telemetry.addData("DETECTED", "Max - Reverse Direction");
 
                     if (gamepad2.right_stick_y > 0) {
                         telemetry.addData("Joystick Y", gamepad2.left_stick_y);
-                        robot.Vert.setPower(gamepad2.left_stick_y);
+                        robot.verticalMotor.setPower(gamepad2.left_stick_y);
 
                     } else {
 
-                        robot.Vert.setPower(0);
+                        robot.verticalMotor.setPower(0);
                     }
-                } else if (robot.Min.isPressed()) {
+                } else if (robot.horizontalLimiter.isPressed()) {
                     telemetry.addData("DETECTED", "Min - Reverse Direction");
 
                     if (gamepad2.right_stick_y < 0) {
@@ -78,7 +245,7 @@ public class Teleop extends LinearOpMode {
 
                     } else {
 
-                        robot.Vert.setPower(0);
+                        robot.verticalMotor.setPower(0);
                     }
                 }
             }//End of Vert control
@@ -86,13 +253,13 @@ public class Teleop extends LinearOpMode {
             //Red and Blue servo control left trigger
                 //position servos to close when trigger pressed, open when released
                 if (gamepad2.left_trigger > 0.25) { //close
-                    robot.Red.setPosition(0.5);
-                    robot.Blue.setPosition(0.5);
+                    robot.redServo.setPosition(0.5);
+                    robot.blueServo.setPosition(0.5);
                     //tbd values
 
                 } else { //open
-                    robot.Red.setPosition(0.5);
-                    robot.Blue.setPosition(0.5);
+                    robot.redServo.setPosition(0.5);
+                    robot.blueServo.setPosition(0.5);
                     //tbd values
                 }
 
@@ -100,22 +267,22 @@ public class Teleop extends LinearOpMode {
             //Orb servo right bumper
                 //bumper pressed, flip out and start intake motor (In)
                 if (gamepad2.right_bumper) {
-                    robot.Org.setPosition(0.5); //tbd value
-                    robot.In.setPower(1); //tbd value for direction
+                    robot.boxServo.setPosition(0.5); //tbd value
+                    robot.intakeMotor.setPower(1); //tbd value for direction
                 } else { //when released, stop intake and flip in
-                    robot.Org.setPosition(0.5); //tbd value
-                    robot.In.setPower(0);
+                    robot.boxServo.setPosition(0.5); //tbd value
+                    robot.intakeMotor.setPower(0);
                 }
 
                 //determine what starts output
                 //ex. use retracted arm touch sensor to initiate intake motor
 
             //Flip servo left bumper
-            
+
                 //when bumper pressed, deliver
                 //when released, return to intake position
 
-
+            */
         }
     }
 }
