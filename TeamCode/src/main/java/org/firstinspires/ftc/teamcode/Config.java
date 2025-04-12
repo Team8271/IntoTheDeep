8400ceaa544352dad792f39770ff9585862f27f4
JunctionDev <187072182+JunctionDev@users.noreply.github.com>
2025-04-11 21:49:12 -0600

    Cleaned up Config.java.
    Renamed components: vertSlide -> liftMotor, slideWithEncoder -> activeSlide, frontTouch -> robotFrontBottomSensor, topTouch -> robotFrontTopSensor, magnetTouch -> liftBottomSensor, horzTouch -> slideSensor.
    
    Removed: slowSpeed, fastSpeed, moveUntilSensor, setSlidePosition, openClaw, closeClaw.
    
    Replaced 'vert' with 'lift' in variables.
    
    Complete comment rework.
    
    Methods moved into classes: Claw, Lift, and Slide. Each class contains methods for corresponding component.
    
    General reformatting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -9,130 +7,127 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
+// tweetybird specific imports
 import dev.narlyx.tweetybird.Drivers.Mecanum;
 import dev.narlyx.tweetybird.Odometers.ThreeWheeled;
 import dev.narlyx.tweetybird.TweetyBird;
 
+/// Robot Configuration Class.
 public class Config {
     private final LinearOpMode opMode;
 
-
-
-
-    // - - - Things that don't need to be changed - - - \\ START
-    public DcMotor fl, fr, bl, br, leftSlide, rightSlide, intakeMotor, vertSlide; //Motors
-    public DcMotor slideWithEncoder; //Used to define which horizontal slide is equipped with encoder wires.
+    public DcMotor fl, fr, bl, br, leftSlide, rightSlide, intakeMotor, liftMotor; // motors
+    public DcMotor activeSlide; // defines which slide motor to use
     public Servo intakeFlip, clawLeft, clawRight, boxServo;
-    public TouchSensor frontTouch, topTouch, magnetTouch, horzTouch;
-    private double clawLeftOpen, clawRightOpen, clawLeftClosed, clawRightClosed; //Claw Values
-    public ThreeWheeled odometer;   //Used for setting up odom pods
-    public Mecanum mecanum;         //Used for mecanum wheels for TweetyBird
-    public TweetyBird tweetyBird;   //Used for TweetyBird setup
+    public TouchSensor robotFrontBottomSensor, robotFrontTopSensor, liftBottomSensor, slideSensor;
+    private double clawLeftOpen, clawRightOpen, clawLeftClosed, clawRightClosed; // claw values
+    public ThreeWheeled odometer;   // odometer pods
+    public Mecanum mecanum;         // mecanum wheels
+    public TweetyBird tweetyBird;   // tweetyBird
 
-    // - - - Things that don't need to be changed - - - \\ END
-    // - - - - - - - - - - - - - - - - - - - - - - - - - \\
-    // - - - Things that can change - - - \\ START
-    public final double intakeCollectPosition   = 0.95,
-            intakeUpPosition        = 0.3,
-            intakeTransferPosition  = 0.1,
-            intakePower             = .8,
-            boxDumpPosition         = 0.83,
-            boxTransferPosition     = 0.07,
-            boxStoragePosition      = 0.2,
-            clawClosedValue         = 0.30, //Smaller # = More Closed (0-1) (left claw)
-            clawOpenValue           = 0.5,  //Larger # = More Open (0-1)  (left claw)
-            slowSpeed               = 0.3,
-            fastSpeed               = 0.4;
-
-    public int          aboveChamber            = 1665,
-            belowChamber            = 1200,
-            wallHeight              = 280,
-            highBasket              = 0,    //This value is not currently used
-            horizontalMax           = 450,
-            verticalMax             = 3300,
-            intakeOnDistance        = 250,  //The distance at which the intake flips down
-            brakeDistance           = 100;  //Horizontal slide stops Braking at this point
-    // - - - Things that can change - - - \\ END
+    public final double intakeCollectPosition  = 0.95,
+                        intakeUpPosition       = 0.3,
+                        intakeTransferPosition = 0.1,
+                        intakePower            = .8,
+                        boxDumpPosition        = 0.83,
+                        boxTransferPosition    = 0.07,
+                        boxStoragePosition     = 0.2,
+                        clawClosedValue        = 0.30, // smaller # = More Closed (0-1) (left claw)
+                        clawOpenValue          = 0.5,  // larger # = More Open (0-1)  (left claw)
 
 
+    public int  liftAboveChamber    = 1665,
+                liftBelowChamber    = 1200,
+                liftWallHeight      = 280,
+                liftHighBasket      = 0,
+                liftMaxHeight       = 3300,
+                slideMaxDistance    = 450,
+                intakeOnDistance    = 250,  // distance the intake flips down to collect
+                slideBrakeDistance  = 100;  // distance the slide has BRAKE mode (ZPB)
 
-    //Don't change this
+    // allow opMode to be referenced
     public Config(LinearOpMode opMode){this.opMode = opMode;}
 
-    //Initialization Method
+    /// Initializes robot components.
     public void init(){
-        //Used to define all motors and servos
+        // hardwareMap to get motors
         HardwareMap hwMap = opMode.hardwareMap;
+
         ///Define Motors
-        //Define the front left wheel
+        // front left wheel
         fl = hwMap.get(DcMotor.class, "FL");
         fl.setDirection(DcMotor.Direction.REVERSE);
         fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Define the front right wheel
+        // front right wheel
         fr = hwMap.get(DcMotor.class, "FR");
         fr.setDirection(DcMotorSimple.Direction.FORWARD);
         fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Define the back left wheel
+        // back left wheel
         bl = hwMap.get(DcMotor.class, "BL");
         bl.setDirection(DcMotor.Direction.REVERSE);
         bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Define the back right wheel
+        // back right wheel
         br = hwMap.get(DcMotor.class, "BR");
         br.setDirection(DcMotor.Direction.FORWARD);
         br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Defining the Horizontal Left Motor
+        // slide left motor
         leftSlide = hwMap.get(DcMotor.class,"HorzLeft");
         leftSlide.setDirection(DcMotor.Direction.FORWARD);
         leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        //Defining the Horizontal Right Motor
+        // slide right motor
         rightSlide = hwMap.get(DcMotor.class,"HorzRight");
         rightSlide.setDirection(DcMotor.Direction.REVERSE);
         rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightSlide.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        // - - - - - - - - - - - - - - - - - - - - - -
-        //Define which horizontal slide has the encoder
-        slideWithEncoder = leftSlide;
+        // define slide motor to use
+        activeSlide = leftSlide;
 
-        //Defining the Intake Motor
+        // intake motor
         intakeMotor = hwMap.get(DcMotor.class,"IntakeMotor");
         intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Defining the Vertical Motor
-        vertSlide = hwMap.get(DcMotor.class,"Vert");
-        vertSlide.setDirection(DcMotor.Direction.REVERSE);
-        vertSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vertSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // lift motor
+        liftMotor = hwMap.get(DcMotor.class,"Vert");
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        ///Define Servos
+        /// Define Servos
         intakeFlip = hwMap.get(Servo.class,"IntakeFlip");
         boxServo = hwMap.get(Servo.class,"BoxServo");
         clawLeft = hwMap.get(Servo.class,"ClawLeft");
         clawRight = hwMap.get(Servo.class,"ClawRight");
 
-        ///Define touch sensors
-        frontTouch = hwMap.get(TouchSensor.class,"FrontTouch");
-        topTouch = hwMap.get(TouchSensor.class,"TopTouch");
-        magnetTouch = hwMap.get(TouchSensor.class,"MagnetTouch");
-        horzTouch = hwMap.get(TouchSensor.class,"HorzTouch");
+        // define values for claw servos
+        clawLeftOpen    = clawOpenValue;
+        clawRightOpen   = 1-clawOpenValue;
+        clawLeftClosed  = clawClosedValue;
+        clawRightClosed = 1-clawClosedValue;
 
+        /// Define touch sensors
+        robotFrontBottomSensor = hwMap.get(TouchSensor.class,"FrontTouch");
+        robotFrontTopSensor = hwMap.get(TouchSensor.class,"TopTouch");
+        liftBottomSensor = hwMap.get(TouchSensor.class,"MagnetTouch");
+        slideSensor = hwMap.get(TouchSensor.class,"HorzTouch");
 
+        // built drive
         mecanum = new Mecanum.Builder()
                 .setFrontLeftMotor(fl)
                 .setFrontRightMotor(fr)
@@ -140,6 +135,7 @@ public class Config {
                 .setBackRightMotor(br)
                 .build();
 
+        // build odometer
         odometer = new ThreeWheeled.Builder()
                 .setLeftEncoder(bl)
                 .setRightEncoder(fr)
@@ -157,22 +153,11 @@ public class Config {
                 .setMiddleEncoderOffset(9.75)
                 .build();
 
+        // reset odometer position
         odometer.resetTo(0,0,0);
-
-
-
-
-
-
-        //Claw stuff and maths
-        clawLeftOpen    = clawOpenValue;
-        clawRightOpen   = 1-clawOpenValue;
-        clawLeftClosed  = clawClosedValue;
-        clawRightClosed = 1-clawClosedValue;
-
     }
 
-    //Sets up and starts tweetybird/odom
+    /// Initializes Tweety Bird.
     public void initTweetyBird(){
         odometer.resetTo(0,0,0);
         tweetyBird = new TweetyBird.Builder()
@@ -188,46 +173,46 @@ public class Config {
                 .build();
     }
 
-    public void closeClaw(){
-        clawLeft.setPosition(clawLeftClosed); //Set left servo to close position
-        clawRight.setPosition(clawRightClosed); //Set right servo to close position
-
-    }
-
-    public void openClaw(){
-        clawLeft.setPosition(clawLeftOpen); //Set left servo to open position
-        clawRight.setPosition(clawRightOpen); //Set right servo to open position
-    }
-
-    public void setSlidePosition(DcMotor slide, int target, double power){
-        slide.setTargetPosition(target);
-        if(slide.getMode() != DcMotor.RunMode.RUN_TO_POSITION){
-            slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    /// Class that contains actions for claw.
+    public class Claw{
+        /// Close the claw.
+        public void close(){
+            clawLeft.setPosition(clawLeftClosed);
+            clawRight.setPosition(clawRightClosed);
         }
-        slide.setPower(power);
-    }
-    public void moveUntilSensor(@NonNull TouchSensor sensor, double speed){
-        //Set all wheels to move at speed
-        fr.setPower(speed);
-        br.setPower(speed);
-        fl.setPower(speed);
-        fr.setPower(speed);
-        opMode.telemetry.addLine("Waiting for " + sensor);
-        opMode.telemetry.update();
-        while(!sensor.isPressed()){
-            opMode.sleep(100); //Waiting
+        /// Open the claw.
+        public void open(){
+            clawLeft.setPosition(clawLeftOpen);
+            clawRight.setPosition(clawRightOpen);
         }
-
-        //This part pushes the bot into the plane more. Ensure bot isn't at angle to wall
-        fr.setPower(.6);
-        br.setPower(.6);
-        fl.setPower(.6);
-        fr.setPower(.6);
-        opMode.sleep(300);
-        //Stop all wheels
-        fr.setPower(0);
-        br.setPower(0);
-        fl.setPower(0);
-        fr.setPower(0);
     }
+
+    /// Class that contains actions for lift.
+    public class Lift{
+        /// Move lift to given position.
+        /// @param target desired position
+        /// @param power motor power
+        public void runToPosition(int target, double power){
+            liftMotor.setTargetPosition(target);
+            if(liftMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION){
+                liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            liftMotor.setPower(power);
+        }
+    }
+
+    /// Class that contains actions for slide.
+    public class Slide{
+        /// Move slide to given position.
+        /// @param target desired position
+        /// @param power motor power
+        public void runToPosition(int target, double power){
+            activeSlide.setTargetPosition(target);
+            if(activeSlide.getMode() != DcMotor.RunMode.RUN_TO_POSITION){
+                activeSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            activeSlide.setPower(power);
+        }
+    }
+
 }
